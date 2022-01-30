12ce119e1d29bafef1101f78946a2c05947a093c
Cosmin <co5myn92@yahoo.com>
2022-01-30 09:42:45 +0200

    update resorvers

--- server/src/graphql/resolvers.js
+++ server/src/graphql/resolvers.js
@@ -1,18 +1,34 @@
+import fetch from 'node-fetch';
+
+const baseURL = 'http://localhost:5000';
+
 const resolvers = {
     Query: {
         async author(root, {id}, context, info) {
             // Fetch an author by ID here...
+            const res = await fetch(`${baseURL}/authors/${id}`).catch(
+                err => err.message === "404: Not Found" && null
+            );
+            return res.json()
         },
         async authors(root, args, context, info) {
             // Fetch all authors here...
+            const res = await fetch(`${baseURL}/authors`);
+            return res.json();
         },
         async book(root, {id}, context, info) {
             // Fetch an book by ID here...
+            const res = await fetch(`${baseURL}/books/${id}`).catch(
+                err => err.message === "404: Not Found" && null
+            );
+            return res.json();
         },
         async books(root, args, context, info) {
             // Fetch all books here...
+            const res = await fetch(`${baseURL}/books`);
+            return res.json();
         }
     }
 }
 
-export const resolvers;
+export default resolvers;
